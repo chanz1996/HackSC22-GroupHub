bf4d1a2413ec06ef09e59c2a10b5dd50b5bc3c7b
chandini <chanz1996@users.noreply.github.com>
2022-02-06 00:14:43 -0800

    Add files via upload
    Javascript file that would be embedded into the Wix website as HTML Iframe to show the chatbot on the main website page.

---
+++ Kommunicate-ChatBot-JavascriptFile.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"18c0a27b03d60a67db2fd9ce9d69375f0","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+</script>
